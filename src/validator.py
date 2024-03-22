3958ea67f05058b5e905abb8d61d4d8376603a8b
LTurret <a0919329@gmail.com>
2024-03-22 14:40:51 +0800

    separate validator from main

---
+++ src/validator.py
@@ -0,0 +1,15 @@
+from os import path
+
+
+def validator(secrets: tuple, model: str) -> bool:
+    if not path.isfile(model):
+        raise FileNotFoundError('"src/model.pkl" is not found!')
+
+    if any(map(lambda credentials: credentials is None, secrets)):
+        raise ValueError(
+            'The provided secrets do not meet the required criteria. Please check your ".env" file and ensure it follows the correct format. For example, SECRET_KEY="YourSecretKey".'
+        )
+    
+    secrets = None
+
+    return True
